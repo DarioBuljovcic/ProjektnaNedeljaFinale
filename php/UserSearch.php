2f4279be8916f8c398cb1125533d0b2d84569266
UltraHardCorn25 <108307199+UltraHardCorn25@users.noreply.github.com>
2023-03-19 13:22:45 +0100

    ---

--- php/UserSearch.php
+++ php/UserSearch.php
@@ -10,7 +10,7 @@
                       
 ?>
 <div class ="singleitemsearchh" name="<?php echo $row['Id']?>">
-    <img src="img/profile2.jpg" alt="" srcset="" id="searchimg">
+    <img src="img/<?php echo $row['img']?>" alt="" srcset="" id="searchimg">
     <h4><?php echo $row['username']?></h4>
     <p><?php echo $row['email']?></p>
     <i class="fa fa-paper-plane-o" style="position:absolute;right:20px;bottom:20px;"></i>
